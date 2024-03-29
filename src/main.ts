40a0ff58e9061b61e5114ab287fa6d7502ca94e4
Marsy24 <marsydev23@gmail.com>
2024-03-30 03:11:35 +0500

    add logos/add fn getPaymentsInfo
    1) Added bank-logos to /public/banks-logos
    2) Added brand-logos to /public/brands-logos
    3) function getPaymentsInfo (utils)

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 import { App } from "./App";
 
+
 document.getElementById('app')?.append(App())
---
+++ src/utils/getPaymentsInfo.ts
@@ -0,0 +1,99 @@
+export type TPaymentsSystems = {
+    [key: string]: TPaymentInfo
+}
+
+export type TPaymentInfo = {
+    nums: number[],
+    path: FPaymentPathImg
+}
+
+export type PaymentInfo = {
+    payment: string,
+    path: FPaymentPathImg | FErrorFindPath,
+    result: boolean
+}
+
+export const paymentsSystems: TPaymentsSystems = {
+    mir: {
+        nums: [2],
+        path: getPath()
+    },
+    'diners club': {
+        nums: [30, 36, 38],
+        path: getPath()
+    },
+    'jcb international': {
+        nums: [31, 35],
+        path: getPath()
+    },
+    'american express': {
+        nums: [34, 37],
+        path: getPath()
+    },
+    visa: {
+        nums: [4],
+        path: getPath()
+    },
+    mastercard: {
+        nums: [51, 52, 53, 54, 55],
+        path: getPath()
+    },
+    maestro: {
+        nums: [50, 56, 57, 58, 63, 67],
+        path: getPath()
+    },
+    discover: {
+        nums: [60],
+        path: getPath()
+    },
+    'china unionpay': {
+        nums: [62],
+        path: getPath()
+    }
+}
+
+export type FPaymentPathImg = (key: string, isOriginal: boolean, isPng: boolean) => string
+export type FErrorFindPath = () => string
+
+function fromKeyToPath(key: string): string {
+    return key.replace(' ', '-');
+}
+
+function getPath(): FPaymentPathImg {
+    return (key: string, isOriginal: boolean = true, isPng: boolean = true) => {
+        if (!key.length) return 'Key is not defined'
+
+        const path = './public/brand-logos/';
+        return isOriginal 
+        ? `${path}${fromKeyToPath(key)}-original.${isPng ? 'png' : 'svg'}`
+        : `${path}${fromKeyToPath(key)}-inverted.${isPng ? 'png' : 'svg'}`
+    }
+
+}
+
+function paySys (this: TPaymentsSystems, ccNum: number): PaymentInfo {
+    const paymentInfo: PaymentInfo = {
+        payment: '',
+        path: () => 'No information found',
+        result: false
+    }
+
+    if (!ccNum || ccNum < 2) return paymentInfo
+
+
+    for (const [key, value] of Object.entries(this)) {
+        value.nums.find(item => {
+            if (item === ccNum) {
+                paymentInfo.payment = key;
+                paymentInfo.path = value.path,
+                paymentInfo.result = !paymentInfo.result
+            }
+        })
+    }
+    
+    return paymentInfo;
+}
+
+const getPaymentsInfo = paySys.bind(paymentsSystems)
+
+export { getPaymentsInfo }
